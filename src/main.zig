9f10d06d934f53ad6bfaed35e1698aea01964caf
Dodo <kasper199914@gmail.com>
2019-03-10 21:19:17 +0100

    ptrtoint stuff?

--- src/main.zig
+++ src/main.zig
@@ -83,86 +83,133 @@ pub fn ArrayVec(comptime T: type, comptime N: usize) type {
         }
 
         fn iter(self: *const Self) type {
-            return struct {
-                ptr: *const T,
-                end: *const T,
+            // return struct {
+            //     ptr: *const T,
+            //     end: *const T,
 
-                const Iter = @This();
+            //     const Iter = @This();
                 
-                fn post_inc_start(this: *Iter, offset: isize) *const T {
-                    // oh well, zero sized types :3
-                    if (comptime @sizeOf(T) == 0) {
-                        this.end = @intToPtr(*const T, (@ptrToInt(this.end)) +% -offset);
-                        return this.ptr;
-                    } else {
-                        var old = this.ptr;
-                        this.ptr = @intToPtr(*const T, @ptrToInt(this.end) + 1);
-                        return old;
-                    }
-                }
+            //     fn post_inc_start(this: *Iter, offset: isize) *const T {
+            //         // oh well, zero sized types :3
+            //         if (comptime @sizeOf(T) == 0) {
+            //             this.end = @intToPtr(*const T, (@ptrToInt(this.end)) +% -offset);
+            //             return this.ptr;
+            //         } else {
+            //             var old = this.ptr;
+            //             this.ptr = @intToPtr(*const T, @ptrToInt(this.end) + 1);
+            //             return old;
+            //         }
+            //     }
 
-                pub fn next(this: *Iter) ?*const T {
-                    if (this.ptr == this.end) {
-                        return null;
-                    } else {
-                        return this.post_inc_start(1);
-                    }
-                }
-            };
+            //     fn next(this: *Iter) ?*const T {
+            //         if (this.ptr == this.end) {
+            //             return null;
+            //         } else {
+            //             return this.post_inc_start(1);
+            //         }
+            //     }
+            // };
+
+            var start: *const T = &self.array[0];
+            var offset = @ptrToInt(start) + self.len();
+
+            const end = @intToPtr(*const T, offset);
+            return build_iter(*const T).init(start, end);
+        }
+    };
+}
+
+fn build_iter(comptime T: type) type {
+    return struct {
+        ptr: T,
+        end: T,
+
+        const Self = @This();
+        
+        fn init(start_ptr: T, end_ptr: T) Self {
+            return Self { .ptr = start_ptr, .end = end_ptr};
+        }
+
+        fn post_inc_start(this: *Self, offset: isize) T {
+            // oh well, zero sized types :3
+            if (comptime @sizeOf(T) == 0) {
+                this.end = @intToPtr(T, (@ptrToInt(this.end)) +% -offset);
+                return this.ptr;
+            } else {
+                var old = this.ptr;
+                this.ptr = @intToPtr(T, @ptrToInt(this.end) + 1);
+                return old;
+            }
+        }
+
+        fn next(this: *Self) T {
+            if (this.ptr == this.end) {
+                return null;
+            } else {
+                return this.post_inc_start(1);
+            }
         }
     };
 }
 
 test "arrayvec push" {
-    comptime var vec = comptime ArrayVec(i32, 4).init();
-    
-    comptime vec.push(1);
-    comptime vec.push(2);
-    comptime vec.push(3);
-    comptime vec.push(4);
+    comptime {
+        var vec = ArrayVec(i32, 4).init();
 
-    comptime debug.assert(vec.len() == 4);
-    comptime debug.assert(std.mem.eql(i32, vec.as_slice(), [4]i32 {1, 2, 3, 4}));
+        vec.push(1);
+        vec.push(2);
+        vec.push(3);
+        vec.push(4);
+
+        debug.assert(vec.len() == 4);
+        debug.assert(std.mem.eql(i32, vec.as_slice(), [4]i32 {1, 2, 3, 4}));
+    }
 }
 
 test "arrayvec pop" {
-    comptime var vec = comptime ArrayVec(i32, 4).init();
+    comptime {
+        var vec = ArrayVec(i32, 4).init();
     
-    comptime vec.push(1);
-    comptime vec.push(2);
-    comptime vec.push(3);
-    comptime vec.push(4);
-    comptime debug.assert(vec.len() == 4);
+        vec.push(1);
+        vec.push(2);
+        vec.push(3);
+        vec.push(4);
+        debug.assert(vec.len() == 4);
 
-    comptime debug.assert(vec.pop().? == 4);    
-    comptime debug.assert(vec.pop().? == 3);
-    comptime debug.assert(vec.pop().? == 2);
-    comptime debug.assert(vec.pop().? == 1);
-    comptime debug.assert(vec.pop() == null);
+        debug.assert(vec.pop().? == 4);    
+        debug.assert(vec.pop().? == 3);
+        debug.assert(vec.pop().? == 2);
+        debug.assert(vec.pop().? == 1);
+        debug.assert(vec.pop() == null);
 
-    comptime debug.assert(vec.len() == 0);
-    comptime debug.assert(std.mem.eql(i32, vec.as_slice(), [0]i32 {}));
+        debug.assert(vec.len() == 0);
+        debug.assert(std.mem.eql(i32, vec.as_slice(), [0]i32 {}));
+    }
 }
 
 test "extend from slice" {
-    comptime var vec = comptime ArrayVec(i32, 10).init();
-
-    comptime vec.push(1);
-
-    comptime var array = [9]i32 {2, 3, 4, 5, 6, 7, 8, 9, 10};
     
-    comptime vec.try_extend_from_slice(&array) catch unreachable;
+    comptime {
+        var vec = ArrayVec(i32, 10).init();
 
-    comptime debug.assert(vec.len() == 10);
+        vec.push(1);
 
-    comptime debug.assert(vec.pop().? == 10);   
+        var array = [9]i32 {2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+        vec.try_extend_from_slice(&array) catch unreachable;
+
+        debug.assert(vec.len() == 10);
+
+        debug.assert(vec.pop().? == 10);
+    }
 }
 
 test "iter" {
-    comptime var vec = comptime ArrayVec(i32, 10).init();
-    comptime var array = [10]i32 {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    comptime vec.try_extend_from_slice(&array) catch unreachable;
-    comptime var iter = vec.iter();
-
-    comptime debug.assert(iter.next().? == 1);
+    comptime {
+        var vec = ArrayVec(i32, 10).init();
+        var array = [10]i32 {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        vec.try_extend_from_slice(&array) catch unreachable;
+        var iter = vec.iter();
+        var nxt = iter.next();
+    }
 }
